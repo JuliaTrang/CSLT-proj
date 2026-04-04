a0856da966530ad10667511025a7b9a7fc70cdfa
TrogHieu <letroghieu06@gmail.com>
2026-04-04 16:45:22 +0700

    [Core] Logic.cpp

---
+++ gamelogic.cpp
@@ -0,0 +1,6 @@
+#include "GameLogic.h"
+
+void handleClick(int x, int y, std::vector<std::vector<TileType>>& mapData, int TILE_SIZE, int width, int height) {
+    int tileX = x / TILE_SIZE;
+    int tileY = y / TILE_SIZE;
+}
